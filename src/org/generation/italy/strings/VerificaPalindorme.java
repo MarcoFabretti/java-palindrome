ce7a366ce2039ebddb253e2d5d1f6a0bca50dea5
FabrettiMarco <mfabretti2000@gmail.com>
2021-11-22 15:16:53 +0100

    Initial commit

---
+++ src/org/generation/italy/strings/VerificaPalindorme.java
@@ -0,0 +1,34 @@
+package org.generation.italy.strings;
+import java.util.Scanner;
+public class VerificaPalindorme {
+
+	public static void main(String[] args) {
+		Scanner cIn = new Scanner(System.in);
+		boolean v=true;
+		char[] parola;
+		System.out.println("Inserire una parola da verificare se palindorma: ");
+		do {
+			System.out.println("Ricorda che la parola deve essere para.");
+		parola = cIn.next().toCharArray();
+		}while(parola.length%2!=0);
+		
+		
+		
+		int j=parola.length-1;	
+		for(int i=0; i<parola.length; i++) {
+				if(!(parola[i]==parola[j])) {
+					v=false;
+				}
+			j--;
+		}
+		
+		
+		
+		
+		
+		if(v==false) {
+			System.out.println("la parola non è palindorma");
+		}else System.out.println("la parola è palindorma");
+	cIn.close();
+	}
+}
