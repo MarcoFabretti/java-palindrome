8400cfe9310f0c7cfc31eac28733f500922a1cb2
FabrettiMarco <mfabretti2000@gmail.com>
2021-11-22 15:43:29 +0100

    corretto-re

--- src/org/generation/italy/strings/VerificaPalindorme.java
+++ src/org/generation/italy/strings/VerificaPalindorme.java
@@ -1,34 +1,25 @@
 package org.generation.italy.strings;
-import java.util.Scanner;
-public class VerificaPalindorme {
 
+import java.util.Scanner;
+
+public class VerificaPalindorme {
 	public static void main(String[] args) {
 		Scanner cIn = new Scanner(System.in);
-		boolean v=true;
+		boolean v = true;
 		char[] parola;
 		System.out.println("Inserire una parola da verificare se palindorma: ");
-		
-		
-		
 		parola = cIn.next().toCharArray();
-		
-		
-		
-		int j=parola.length-1;	
-		
-		
-		
-		for(int i=0; i<parola.length; i++) {
-				if(!(parola[i]==parola[j])) {
-					v=false;
-				}
+		int j = parola.length - 1;
+		for (int i = 0; i < parola.length; i++) {
+			if (!(parola[i] == parola[j])) {
+				v = false;
+			}
 			j--;
 		}
-		
-		
-		if(v==false) {
-			System.out.println("la parola non è palindorma");
-		}else System.out.println("la parola è palindorma");
-	cIn.close();
+		if (v == false) {
+			System.out.println("la parola non è palindorma ed è lunga: " + parola.length);
+		} else
+			System.out.println("la parola è palindorma ed è lunga: " + parola.length);
+		cIn.close();
 	}
 }
