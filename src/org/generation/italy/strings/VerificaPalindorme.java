eeabcd788963f5d3e3c7e9ac5f0eed010df7412f
FabrettiMarco <mfabretti2000@gmail.com>
2021-11-22 15:28:03 +0100

    corretto

--- src/org/generation/italy/strings/VerificaPalindorme.java
+++ src/org/generation/italy/strings/VerificaPalindorme.java
@@ -7,23 +7,23 @@ public class VerificaPalindorme {
 		boolean v=true;
 		char[] parola;
 		System.out.println("Inserire una parola da verificare se palindorma: ");
-		do {
-			System.out.println("Ricorda che la parola deve essere para.");
+		
+		
+		
 		parola = cIn.next().toCharArray();
-		}while(parola.length%2!=0);
 		
 		
 		
 		int j=parola.length-1;	
+		
+		
+		
 		for(int i=0; i<parola.length; i++) {
 				if(!(parola[i]==parola[j])) {
 					v=false;
 				}
 			j--;
 		}
-		
-		
-		
 		
 		
 		if(v==false) {
